d2c7170ade2f2cd00447cc4248340478d5b5913b
ItzPolarBearDK <70688416+ItzPolarBearDK@users.noreply.github.com>
2025-01-16 10:28:01 +0100

    Updated frontend - added login

---
+++ FlagSpillet/Components/Models.cs
@@ -0,0 +1,28 @@
+﻿namespace FlagSpillet.Components
+{
+    public class UserDTO
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+    }
+    public class Name
+    {
+        public string common { get; set; }
+        public string official { get; set; }
+    }
+    public class Flag
+    {
+        public string png { get; set; }
+    }
+    public class Country
+    {
+        public Flag flags { get; set; }
+        public Name name { get; set; }
+    }
+    public class LoginResult
+    {
+        public string Username { get; set; }
+        public string ID { get; set; }
+    }
+}
